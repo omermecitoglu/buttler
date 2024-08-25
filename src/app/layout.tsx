aeba1b5d894d7f6c1164c6d04582287409507ab2
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-25 17:26:32 +0700

    chore: improve layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import "~/styles/custom-bootstrap.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import React, { type ReactNode } from "react";
+import CardBody from "react-bootstrap/CardBody";
 import Container from "react-bootstrap/Container";
+import Tabs from "~/components/Tabs";
 
 config.autoAddCss = false;
 
@@ -15,8 +17,13 @@ const RootLayout = ({
 }: RootLayoutProps) => (
   <html lang="en">
     <body>
-      <Container as="main">
-        {children}
+      <Container as="main" className="pt-3">
+        <Tabs />
+        <div className="rounded-bottom border-start border-end border-bottom shadow p-3">
+          <CardBody>
+            {children}
+          </CardBody>
+        </div>
       </Container>
     </body>
   </html>
