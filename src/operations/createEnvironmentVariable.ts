f6752a182fccec140a64797cb6475e34130aacdf
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-26 16:17:06 +0700

    chore: delete env operations

--- src/operations/createEnvironmentVariable.ts
+++
@@ -1,9 +0,0 @@
-import type database from "~/database";
-import { environmentVariables } from "~/database/schema/environment-variables";
-import type { NewEnvironmentVariableDTO } from "~/models/environment-variable";
-import type z from "zod";
-
-export default async function createEnvironmentVariable(db: typeof database, data: z.infer<typeof NewEnvironmentVariableDTO>) {
-  const [environmentVariable] = await db.insert(environmentVariables).values(data).returning({ id: environmentVariables.id });
-  return environmentVariable;
-}
--- src/operations/getEnvironmentVariable.ts
+++
@@ -1,7 +0,0 @@
-import type database from "~/database";
-
-export default function getEnvironmentVariable(db: typeof database, environmentVariableId: string) {
-  return db.query.environmentVariables.findFirst({
-    where: (table, { eq }) => eq(table.id, environmentVariableId),
-  });
-}
