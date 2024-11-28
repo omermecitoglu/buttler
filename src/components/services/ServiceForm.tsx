73eafcb65f164579a6b85e9cf42671a740f2e686
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-11-28 19:02:55 +0700

    refactor: service form with database kind

--- src/components/services/ServiceForm.tsx
+++ src/components/services/ServiceForm.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Input from "@omer-x/bs-ui-kit/form/Input";
-import React from "react";
+import Select from "@omer-x/bs-ui-kit/form/Select";
+import React, { useState } from "react";
 import type { ServiceDTO } from "~/models/service";
 import type z from "zod";
 
@@ -9,23 +11,64 @@ type ServiceFormProps = {
 
 const ServiceForm = ({
   data,
-}: ServiceFormProps) => (
-  <>
-    <Input
-      label="Name"
-      name="name"
-      required
-      autoFocus
-      defaultValue={data?.name}
-    />
-    <Input
-      label="Git Repository"
-      name="repo"
-      required
-      defaultValue={data?.repo}
-      readOnly={(data?.repo ?? "").length > 0}
-    />
-  </>
-);
+}: ServiceFormProps) => {
+  const [kind, setKind] = useState<z.infer<typeof ServiceDTO>["kind"]>(data?.kind ?? "git");
+
+  const dbEngineName = (engine: "mysql" | "postgres" | "mongo" | "redis") => {
+    switch (engine) {
+      case "postgres": return "PostgreSQL";
+      case "mysql": return "MySQL";
+      case "mongo": return "MongoDB";
+      case "redis": return "Redis";
+    }
+  };
+
+  return (
+    <>
+      {!data && (
+        <Select
+          label="Type"
+          name="kind"
+          options={[
+            "git",
+            "database",
+          ]}
+          optionName={option => (option === "git" ? "Custom" : "Database")}
+          value={kind}
+          onChange={setKind}
+        />
+      )}
+      <Input
+        label="Name"
+        name="name"
+        required
+        autoFocus
+        defaultValue={data?.name}
+      />
+      {(kind === "git" || !!data) && (
+        <Input
+          label="Repository"
+          name="repo"
+          required
+          defaultValue={data?.repo}
+          readOnly={(data?.repo ?? "").length > 0}
+        />
+      )}
+      {kind === "database" && !data && (
+        <Select
+          label="Engine"
+          name="repo"
+          options={[
+            "postgres",
+            "mysql",
+            "mongo",
+            "redis",
+          ]}
+          optionName={dbEngineName}
+        />
+      )}
+    </>
+  );
+};
 
 export default ServiceForm;
