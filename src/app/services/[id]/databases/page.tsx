25a3bf3bfd3223d1a855da2ed925e2acb56dc350
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-11-30 11:19:48 +0700

    refactor: service databases page with list

--- src/app/services/[id]/databases/page.tsx
+++ src/app/services/[id]/databases/page.tsx
@@ -1,13 +1,16 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import DataTable from "@omer-x/bs-ui-kit/DataTable";
 import PageSection from "@omer-x/bs-ui-kit/PageSection";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
-import { attachDatabase } from "~/actions/service";
+import { attachDatabase, detachDatabase } from "~/actions/service";
 import BackButton from "~/components/BackButton";
 import ModalList from "~/components/ModalList";
 import db from "~/database";
 import getService from "~/operations/getService";
 import getServices from "~/operations/getServices";
+import { pluck } from "~/utils/object";
 
 type ServiceDatabasesPageProps = {
   params: {
@@ -22,7 +25,8 @@ const ServiceDatabasesPage = async ({
   const service = await getService(db, params.id);
   if (!service) notFound();
   const allServices = await getServices(db, ["id", "kind", "name"]);
-  const allDatabases = allServices.filter(s => s.kind === "database");
+  const alreadyProvidedDatabases = pluck(service.providers, "id");
+  const allDatabases = allServices.filter(s => (s.kind === "database" && !alreadyProvidedDatabases.includes(s.id)));
   const availableDatabases = Object.fromEntries(allDatabases.map(s => [s.id, s.name] as const));
   return (
     <PageSection
@@ -43,9 +47,28 @@ const ServiceDatabasesPage = async ({
         </>
       )}
     >
-      <span className="text-muted fst-italic">
-        No databases attached to this service.
-      </span>
+      {service.providers.length ? (
+        <DataTable
+          link={Link}
+          collection={service.providers}
+          primaryKey="id"
+          schema={{
+            name: {
+              header: "Name",
+            },
+          }}
+          destroyAction={detachDatabase.bind(null, service.id)}
+          destroyWarningTitle="Delete Service"
+          destroyWarningDescription="Are you sure you want to detach this database?"
+          destroyConfirmText="Confirm"
+          destroyCancelText="Cancel"
+          destroyDisabled={false}
+        />
+      ) : (
+        <span className="text-muted fst-italic">
+          No databases attached to this service.
+        </span>
+      )}
     </PageSection>
   );
 };
