0f3210c4b642305620e095de51fd213bd2d7abc4
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-12-16 10:27:12 +0700

    refactor: webhook with event name

--- src/app/webhook/route.ts
+++ src/app/webhook/route.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { startBuilding } from "~/core/build";
 import { verifySignature } from "~/core/github";
 import db from "~/database";
@@ -12,14 +13,24 @@ type WekHookEvent = {
 export async function POST(request: Request) {
   const buffer = await request.arrayBuffer();
   const isSafe = verifySignature(request, buffer, "buttler");
-  if (!isSafe) {
+  if (!isSafe && process.env.NODE_ENV === "production") {
     return new Response("Unauthorized", { status: 401 });
   }
-  const event = JSON.parse(Buffer.from(buffer).toString()) as WekHookEvent;
-
-  const service = await getServiceByRepo(db, event.repository.ssh_url);
-  if (service) {
-    await startBuilding(service);
+  const headersList = await headers();
+  const eventName = headersList.get("X-GitHub-Event");
+  switch (eventName) {
+    case "ping": {
+      // pong
+      break;
+    }
+    case "push": {
+      const event = JSON.parse(Buffer.from(buffer).toString()) as WekHookEvent;
+      const service = await getServiceByRepo(db, event.repository.ssh_url);
+      if (service) {
+        await startBuilding(service);
+      }
+      break;
+    }
   }
   return new Response("Webhook received", { status: 200 });
 }
