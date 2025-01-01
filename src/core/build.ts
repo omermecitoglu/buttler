798dd2bc940b46fc71dab122e0bb968424d2a980
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-01-01 09:42:48 +0700

    refactor: startBuilding with error handler

--- src/core/build.ts
+++ src/core/build.ts
@@ -1,6 +1,7 @@
 import "server-only";
 import { kebabCase } from "change-case";
 import { after } from "next/server";
+import { GitError } from "simple-git";
 import { buildImage, createContainer, removeContainer } from "~/core/docker";
 import { cloneRepo, deleteRepo } from "~/core/git";
 import db from "~/database";
@@ -13,27 +14,35 @@ import { getProviderVariables } from "./provider";
 export async function startBuilding(service: ServiceDTO) {
   const image = await createBuildImage(db, { serviceId: service.id });
   after(async () => {
-    const repoPath = await cloneRepo(service.repo, service.id);
-    const success = await buildImage(image.id, repoPath, false);
-    await deleteRepo(service.id);
-    await updateBuildImage(db, image.id, { status: success ? "ready" : "failed" });
+    try {
+      const repoPath = await cloneRepo(service.repo, service.id);
+      const success = await buildImage(image.id, repoPath, false);
+      await deleteRepo(service.id);
+      await updateBuildImage(db, image.id, { status: success ? "ready" : "failed" });
 
-    // update container
-    if (service.containerId) {
-      await removeContainer(service.containerId);
-      await updateService(db, service.id, { status: "idle", containerId: null, imageId: null });
-      const providerVariables = service.providers
-        .map(provider => getProviderVariables(service.name, provider.name, provider.variables))
-        .reduce((bundle, current) => Object.assign(bundle, current), {});
-      const containerId = await createContainer(
-        kebabCase(service.name),
-        image.id,
-        { ...providerVariables, ...service.environmentVariables },
-        service.ports,
-        service.volumes,
-        service.providers.map(provider => provider.networkIds).flat(),
-      );
-      await updateService(db, service.id, { status: "running", containerId, imageId: image.id });
+      // update container
+      if (service.containerId) {
+        await removeContainer(service.containerId);
+        await updateService(db, service.id, { status: "idle", containerId: null, imageId: null });
+        const providerVariables = service.providers
+          .map(provider => getProviderVariables(service.name, provider.name, provider.variables))
+          .reduce((bundle, current) => Object.assign(bundle, current), {});
+        const containerId = await createContainer(
+          kebabCase(service.name),
+          image.id,
+          { ...providerVariables, ...service.environmentVariables },
+          service.ports,
+          service.volumes,
+          service.providers.map(provider => provider.networkIds).flat(),
+        );
+        await updateService(db, service.id, { status: "running", containerId, imageId: image.id });
+      }
+    } catch (error) {
+      if (error instanceof GitError && error.message.includes("Repository not found")) {
+        await updateBuildImage(db, image.id, { status: "failed", errorCode: "REPO_NOT_FOUND" });
+      } else {
+        await updateBuildImage(db, image.id, { status: "failed" });
+      }
     }
   });
   return image;
