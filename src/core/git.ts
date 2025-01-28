7ad1c6e0e69d65bf8352cdf1acd956f3c8193559
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-01-28 17:03:26 +0300

    fix: git clone with latest tagged commit

--- src/core/git.ts
+++ src/core/git.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import simpleGit, { type SimpleGit } from "simple-git";
+import simpleGit from "simple-git";
 import env from "./env";
 
 function getRepoPath(serviceId: string) {
@@ -16,28 +16,16 @@ export async function cloneRepo(repoUrl: string, serviceId: string) {
     binary: "git",
     maxConcurrentProcesses: 6,
   });
-  const latestTaggedCommitHash = await getLatestTaggedCommitHash(git, repoUrl);
   await git.clone(repoUrl, repoPath, {
     "--depth": "1",
     "--single-branch": null,
     "--branch": "main",
   });
-  if (latestTaggedCommitHash) {
-    await git.cwd(repoPath);
-    await git.fetch("origin", latestTaggedCommitHash);
-  }
+  await git.cwd(repoPath);
+  await git.fetch("--tags");
   return repoPath;
 }
 
 export async function deleteRepo(serviceId: string) {
   await fs.rm(getRepoPath(serviceId), { recursive: true, force: true });
-}
-
-async function getLatestTaggedCommitHash(git: SimpleGit, repoUrl: string) {
-  const output = await git.listRemote(["--tags", repoUrl]);
-  const entries = output.split("\n").slice(0, -1).map(entry => entry.split("\t"));
-  const lastEntry = entries.at(-1);
-  if (lastEntry && lastEntry.length === 2) {
-    return lastEntry[0];
-  }
 }
