834e2725b8b4c951c81189a2cc1acfa9b43b32d2
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-11 15:16:41 +0700

    internal: hidden storage utils

---
+++ src/core/storage.ts
@@ -0,0 +1,27 @@
+import { constants } from "fs";
+import fs from "node:fs/promises";
+import { homedir } from "node:os";
+import path from "node:path";
+
+const buttlerDirectory = path.resolve(homedir(), ".buttler");
+
+export function getFilePath(dir: string, fileName: string) {
+  return path.resolve(buttlerDirectory, dir, fileName);
+}
+
+export async function checkFile(dir: string, fileName: string) {
+  try {
+    await fs.access(getFilePath(dir, fileName), constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export async function saveFile(dir: string, fileName: string, content: string) {
+  const dirPath = path.resolve(buttlerDirectory, dir);
+  await fs.mkdir(dirPath, { recursive: true });
+  const filePath = path.resolve(dirPath, fileName);
+  await fs.writeFile(filePath, content, "utf-8");
+  return filePath;
+}
