dfd0414fa40f64677a5f99354a45b5e49aab5e02
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-15 14:38:51 +0700

    build: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
+    authInterrupts: true,
   },
   sassOptions: {
     silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import", "legacy-js-api"],
   },
+  allowedDevOrigins: ["*.ngrok-free.app"],
+  poweredByHeader: false,
   webpack: config => {
     config.externals.push("dockerode");
     return config;
